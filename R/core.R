7cd3abf1cf806ae8c247e9022c2047589cc4a415
Konrad Grzanek <kongra@gmail.com>
2015-07-10 21:35:23 +0200

    Fixed exports.

--- R/core.R
+++ R/core.R
@@ -2,14 +2,18 @@
 # Created 2015-07-08
 
 #' @import dplyr
+NULL
 
 #' Alias for \code{pryr::object_size}
+#' @export
 osize  <- pryr::object_size
 
 #' Alias for \code{pryr::mem_used}
+#' @export
 memuse <- pryr::mem_used
 
 #' Alias for \code{microbenchmark::microbenchmark}
+#' @export
 timeit <- microbenchmark::microbenchmark
 
 #' Execute gc multiple times.
@@ -18,6 +22,7 @@ timeit <- microbenchmark::microbenchmark
 #'
 #' @param n Number of calls.
 #' @return The result of the last gc() call.
+#' @export
 rgc <- function(n = 10) {
   if (n > 1) {
     for (i in 1:(n - 1)) {
@@ -34,6 +39,7 @@ rgc <- function(n = 10) {
 #'
 #' @param prefix Prefix for the condition message.
 #' @return A condition handler.
+#' @export
 printCondition <- function(prefix = "ERROR: ") {
   function(e) {
     print(paste(prefix, conditionMessage(e), sep = ""))
@@ -42,21 +48,25 @@ printCondition <- function(prefix = "ERROR: ") {
 
 #' Returns a data-frame of n most frequent elements in vector x
 #' together with their frequencies.
+#' @export
 mostFrequent <- function(x, n = 10) {
   data_frame(x) %>% group_by(x) %>% summarise(freq = n()) %>%
     arrange(desc(freq)) %>% head(n)
 }
 
 #' Returns a data-frame of mode values, together with their frequency.
+#' @export
 modes <- function(x) {
   df <- data_frame(x) %>% group_by(x) %>% summarise(freq = n())
   df %>% filter(freq == max(df$freq))
 }
 
 #' Defaults for NULL values.
+#' @export
 `%or%` <- function(x, y) if (is.null(x)) y else x
 
 #' Remove NULLs from a list.
+#' @export
 compact <- function(x) {
   x[!vapply(x, is.null, logical(1))]
 }
