da804b6c2dda358b1d7075f22bd80d2f6d4f393e
Konrad Grzanek <kongra@gmail.com>
2015-07-20 23:09:46 +0200

    Added duplicatedBy

--- R/data.R
+++ R/data.R
@@ -1,29 +1,54 @@
 # Copyright (c) Konrad Grzanek
 # Created 2015-07-20
 
+#' @import data.table
+NULL
+
 #' Reports the number of elements matching the predicate.
 #'
-#' @param  .data The data object (vector, frame).
+#' @param  x The data object (vector, frame).
 #' @param  pred The predicate.
 #' @param  keepZeros For non-atomics. When TRUE the zero counts will be
 #'                   kept.
 #' @return A representation of counts.
 #' @export
-countMatching <- function(.data, pred, ...) {
+countMatching <- function(x, pred, ...) {
   UseMethod("countMatching")
 }
 
 #' @export
-countMatching.default <- function(.data, pred, ...) {
-  sum(pred(.data))
+countMatching.default <- function(x, pred, ...) {
+  sum(pred(x))
 }
 
 #' @export
-countMatching.data.frame <- function(.data, pred, keepZeros = FALSE) {
+countMatching.data.frame <- function(x, pred, keepZeros = FALSE) {
   result <- list()
-  for (n in colnames(.data)) {
-    v <- sum(pred(.data[[n]]))
+  for (n in colnames(x)) {
+    v <- sum(pred(x[[n]]))
     if (keepZeros || v != 0) result[[n]] <- v
   }
   result
+}
+
+#' Determine and return duplicated rows.
+#'
+#' \code{duplicatedBy} returns rows having same values in the specified by col.
+#' @export
+duplicatedBy <- function(x, by, ...) {
+  UseMethod("duplicatedBy")
+}
+
+#' @export
+duplicatedBy.data.table <- function(dt, by, ...) {
+  i    <- which(duplicated(dt[[by]], ...))
+  dups <- dt[i][[by]]
+  dt[dt[[by]] %in% dups]
+}
+
+#' @export
+duplicatedBy.data.frame <- function(df, by, ...) {
+  i    <- which(duplicated(df[[by]], ...))
+  dups <- df[i, by]
+  df[df[[by]] %in% dups, ]
 }
