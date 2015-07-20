2aeb707308f52ef349b2b6ed1892f5caffa1293a
Konrad Grzanek <kongra@gmail.com>
2015-07-20 17:30:25 +0200

    Introduced countMatching

---
+++ R/data.R
@@ -0,0 +1,27 @@
+# Copyright (c) Konrad Grzanek
+# Created 2015-07-20
+
+#' Reports the number of elements matching the predicate.
+#'
+#' @param  .data The data object (vector, frame).
+#' @param  pred The predicate.
+#' @param  keepZeros For non-atomics. When TRUE the zero counts will be
+#'                   kept.
+#' @return A representation of counts.
+#' @export
+countMatching <- function(.data, pred, ...) {
+  UseMethod("countMatching")
+}
+
+countMatching.default <- function(.data, pred, ...) {
+  sum(pred(.data))
+}
+
+countMatching.data.frame <- function(.data, pred, keepZeros = FALSE) {
+  result <- list()
+  for(n in colnames(.data)) {
+    v <- sum(pred(.data[[n]]))
+    if(keepZeros || v != 0) result[[n]] <- v
+  }
+  result
+}
