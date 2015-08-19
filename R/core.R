02c2d053b21eb2d1667fe173683f40cacf01fb40
Konrad Grzanek <kongra@gmail.com>
2015-08-19 10:41:54 +0200

    Added hashmap constructor.

--- R/core.R
+++ R/core.R
@@ -77,3 +77,8 @@ modes <- function(x) {
 compact <- function(x) {
   x[!vapply(x, is.null, logical(1))]
 }
+
+#' Creates an empty hashmap (environment).
+#' @param size See new.env documentation
+#' @export
+hashmap <- function(size = NA) new.env(parent = emptyenv(), size = size)
