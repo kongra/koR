a15a58dbc307a65d459770b49d7897a32ac7787e
Konrad Grzanek <kongra@gmail.com>
2016-12-07 13:01:28 +0100

    Improvements

--- src/data.cpp
+++ src/data.cpp
@@ -10,18 +10,17 @@ using namespace Rcpp;
 //' @export
 // [[Rcpp::export]]
 double skewnessRcpp(NumericVector x) {
-  double mean = Rcpp::mean(x);
-  int n       = x.size();
-
+  const double mean = Rcpp::mean(x);
+  const int n       = x.size();
   double squaresSum = 0;
   double cubesSum   = 0;
 
   for(int i = 0; i < n; i++) {
-    double r   = x[i] - mean;
-    double s   = r * r;
-    squaresSum += s;
-    cubesSum   += (s * r);
+    const double r = x[i] - mean;
+    const double s = r * r;
+    squaresSum    += s;
+    cubesSum      += (s * r);
   }
-  double v = squaresSum / (n - 1);
+  const double v = squaresSum / (n - 1);
   return cubesSum / ((n - 1) * sqrt(v * v * v));
 }
