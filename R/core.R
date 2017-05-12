9f70e38564bcf691bf016773f751c75a7b1a74f5
Konrad Grzanek <kongra@gmail.com>
2017-05-12 21:34:49 +0200

    Cleanup, removing unnecessary stuff, making use of chR where possible.

--- R/core.R
+++ R/core.R
@@ -2,6 +2,7 @@
 # Created 2015-07-08
 
 #' @import data.table
+#' @import chR
 NULL
 
 #' Alias for \code{pryr::object_size}
@@ -27,14 +28,16 @@ qbench <- microbenchmark::microbenchmark
 #' @param n Number of calls.
 #' @return The result of the last gc() call.
 #' @export
-rgc <- function(n = 10) {
+rgc <- function(n = 10L) chUnit({
+  chPosInt(n)
   if (n > 1) {
     for (i in 1:(n - 1)) {
       gc()
     }
   }
   gc()
-}
+  NULL
+})
 
 #' Condition handler generator.
 #'
@@ -44,11 +47,12 @@ rgc <- function(n = 10) {
 #' @param prefix Prefix for the condition message.
 #' @return A condition handler.
 #' @export
-printCondition <- function(prefix = "ERROR: ") {
+printCondition <- function(prefix = "ERROR: ") chFun({
+  chString(prefix)
   function(e) {
     print(paste(prefix, conditionMessage(e), sep = ""))
   }
-}
+})
 
 #' Debug wrapper generator.
 #'
@@ -57,57 +61,52 @@ printCondition <- function(prefix = "ERROR: ") {
 #' @param f Function to wrap
 #' @param prefix A prefix for the diagnostic message
 #' @export
-chatty <- function(f, prefix = "Processing ") {
+chatty <- function(f, prefix = "Processing ") chFun({
+  chFun(f)
+  chString(prefix)
   function(x, ...) {
     res <- f(x, ...)
     cat(prefix, x, "\n", sep = "")
     res
   }
-}
+})
 
 #' Returns a data.table of n most frequent elements in vector x
 #' together with their frequencies.
 #' @export
-mostFrequent <- function(x, n = 10) {
+mostFrequent <- function(x, n = 10) chDT({
+  chVector(x)
   dt <- data.table(x = x)
   head(dt[, .(freq = .N), by = x][order(-freq)], n)
-}
+})
 
 #' Returns a data.table of mode values, together with their frequency.
 #' @export
-modes <- function(x) {
-  dt <- data.table(x = x)[, .(freq = .N), by = x]
-  maxFreq = max(dt$freq)
+modes <- function(x) chDT({
+  chVector(x)
+  dt      <- data.table(x = x)[, .(freq = .N), by = x]
+  maxFreq <- max(dt$freq)
   dt[freq == maxFreq]
-}
+})
 
 #' Defaults for NULL values.
 #' @export
 `%or%` <- function(x, y) if (is.null(x)) y else x
 
-#' Remove NULLs from a list.
-#' @param x The list
-#' @export
-compact <- function(x) {
-  x[!vapply(x, is.null, logical(1))]
-}
-
-#' Creates an empty hashmap (environment).
-#' @param size See new.env documentation
-#' @export
-hashmap <- function(size = NULL) new.env(parent = emptyenv(), size = size)
-
 ##' Gets user installed packages.
 ##' @return a data table containing all the relevant information
 ##' @seealso installed.packages()
 ##' @export
-userInstalledPackages <- function()
+userInstalledPackages <- function() chDT({
   as.data.table(installed.packages())[is.na(Priority)]
+})
 
 ##' Returns a a decorator of f that prints . every n.
 ##' @export
 dotEvery <- function(n, f) {
-  i <- 1
+  chPosInt(n)
+  chFun(f)
+  i <- 1L
   function(...) {
     if (i %% n == 0) cat(".")
     i <<- i + 1
@@ -117,9 +116,11 @@ dotEvery <- function(n, f) {
 
 ##' Returns a decorator of f that sleeps time (in seconds) before executing f.
 ##' @export
-sysSleeping <- function(time, f) {
+sysSleeping <- function(time, f) chFun({
+  chPosInt(time)
+  chFun(f)
   function(...) {
     Sys.sleep(time)
     f(...)
   }
-}
+})
