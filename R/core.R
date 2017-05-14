7ba5bf852b315310a1d787d4f653a66d52a5619c
Konrad Grzanek <kongra@gmail.com>
2017-05-14 08:24:50 +0200

    Created tools for more convenient creation of lists/env maps using key/value pairs.

--- R/core.R
+++ R/core.R
@@ -93,16 +93,16 @@ modes <- function(x) chDT({
 #' @export
 `%or%` <- function(x, y) if (is.null(x)) y else x
 
-##' Gets user installed packages.
-##' @return a data table containing all the relevant information
-##' @seealso installed.packages()
-##' @export
+#' Gets user installed packages.
+#' @return a data table containing all the relevant information
+#' @seealso installed.packages()
+#' @export
 userInstalledPackages <- function() chDT({
   as.data.table(installed.packages())[is.na(Priority)]
 })
 
-##' Returns a a decorator of f that prints . every n.
-##' @export
+#' Returns a a decorator of f that prints . every n.
+#' @export
 dotEvery <- function(n, f) {
   chPosInt(n)
   chFun(f)
@@ -114,8 +114,8 @@ dotEvery <- function(n, f) {
   }
 }
 
-##' Returns a decorator of f that sleeps time (in seconds) before executing f.
-##' @export
+#' Returns a decorator of f that sleeps time (in seconds) before executing f.
+#' @export
 sysSleeping <- function(time, f) chFun({
   chPosInt(time)
   chFun(f)
@@ -123,4 +123,39 @@ sysSleeping <- function(time, f) chFun({
     Sys.sleep(time)
     f(...)
   }
+})
+
+#' Puts all key/value mapping pairs into m
+#' @param m associative container, list or env
+#' @param kvs list of key/value pairs (must be even in size)
+#' @return updated m
+#' @export
+assocKvs <- function(m, kvs) chRecursive({
+  chRecursive(m)
+  chList   (kvs)
+  n   <- chEvenInt(length(kvs))
+  if (n > 0L) {
+    for (i in (1:(n/2))) {
+      k <- kvs[[2 * i - 1]]
+      v <- kvs[[2 * i]]
+      m[[k]] <- v
+    }
+  }
+  m
+})
+
+#' Creates a list using the enumerated collection of key/value mapping pairs
+#' @param ... collection of key/value pairs (must be even in size)
+#' @return the resulting list
+#' @export
+createListMap <- function(...) chList({
+  assocKvs(list(), list(...))
+})
+
+#' Creates an env using the enumerated collection of key/value mapping pairs
+#' @param ... collection of key/value pairs (must be even in size)
+#' @return the resulting env
+#' @export
+createEnvMap <- function(...) chEnv({
+  assocKvs(new.env(hash = TRUE), list(...))
 })
