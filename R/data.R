fc45054d9eb49e5d87602d747496a9ccbeeafa59
Konrad Grzanek <kongra@gmail.com>
2017-06-01 09:44:51 +0200

    Added some data.table utilities.

--- R/data.R
+++ R/data.R
@@ -1,2 +1,39 @@
 # Copyright (c) Konrad Grzanek
 # Created 2015-07-20
+
+#' @import data.table
+#' @import purrr
+#' @import chR
+NULL
+
+#' Binds the data.table arguments into one data.table, optionally filling
+#' @return the resulting data.table
+#' @export
+bindDTs <- function(..., fill = FALSE) chDT({
+  chBool(fill)
+  result <- NULL
+  for (dt in list(...)) {
+    chDT(dt)
+    if (is.null(result))
+      result <- dt
+    else
+      result <- rbindlist(list(result, dt), fill = fill)
+  }
+  result
+})
+
+#' Maps f over each singleton data.tables representing the rows of dt
+#' @param dt chDT
+#' @param f chFun chDT1 chDT1
+#' @return chDT
+#' @export
+mapDT <- function(dt, f) chDT({
+  chDT(dt)
+  chFun(f)
+  result <- data.table()
+  by(dt, seq_len(nrow(dt)), function (rowDT) {
+    result <<- rbindlist(list(result, rowDT %>% f %>% chDT1), fill = TRUE)
+  })
+  result
+})
+
