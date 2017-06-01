64f7ead1ac4e4e8111d95a5e2b1cbdb4a660b77a
Konrad Grzanek <kongra@gmail.com>
2017-06-01 10:13:16 +0200

    Functional fixture

--- R/data.R
+++ R/data.R
@@ -27,12 +27,17 @@ bindDTs <- function(..., fill = FALSE) chDT({
 #' @param f chFun chDT1 chDT1
 #' @return chDT
 #' @export
-mapDT <- function(dt, f) chDT({
+mapDT <- function(dt, f, na.rm = TRUE) chDT({
   chDT(dt)
   chFun(f)
   result <- data.table()
   by(dt, seq_len(nrow(dt)), function (rowDT) {
-    result <<- rbindlist(list(result, rowDT %>% f %>% chDT1), fill = TRUE)
+    r <- f(rowDT)
+    if (is.na(r) || is.null(r))
+      if (!na.rm) chDT1(r) # False assertion here
+    else
+      # We require the resulting r to have exactly 1 row, empty r should be NULL/NA
+      result <- rbindlist(list(result, chDT1(r)), fill = TRUE)
   })
   result
 })
