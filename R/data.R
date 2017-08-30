34d81a0517ce0983a9bf1ed30bbb312a992f9388
kongra <kongra@gmail.com>
2017-08-30 10:35:23 +0200

    Added moveNames.

--- R/data.R
+++ R/data.R
@@ -2,6 +2,7 @@
 # Created 2015-07-20
 
 #' @import data.table
+#' @import assertthat
 #' @import chR
 NULL
 
@@ -42,4 +43,30 @@ reduceDTcols <- function(dt, cols, f, ...) chVector({
   chStrings(cols)
   chFun(f)
   purrr::reduce(as.list(cols), function(x, c) f(x, dt[[c]]), ...)
+})
+
+#'Thanks to: https://stackoverflow.com/questions/18339370/reordering-columns-in-a-large-dataframe
+#' @export
+moveNames <- function(names, tomove, where = "last", col = NULL) chStrings({
+  chStrings(names)
+  chStrings(tomove)
+  chString(where)
+  chMaybe(chString, col)
+
+  assert_that(all(tomove %in% names))
+
+  temp <- setdiff(names, tomove)
+  switch(where,
+         first = c(tomove, temp),
+         last  = c(temp, tomove),
+         before = {
+           if (is.null(col)) stop("col must be specified when using `before`")
+           assert_that(col %in% names)
+           append(temp, values = tomove, after = (match(col, temp)-1))
+         },
+         after = {
+           if (is.null(col)) stop("col must be specified when using `after`")
+           assert_that(col %in% names)
+           append(temp, values = tomove, after = (match(col, temp)))
+         })
 })
