0689fbc253d8dc6c9174b2ccd89c41113abb32df
kongra <kongra@gmail.com>
2018-01-28 21:58:24 +0100

    stampString improvement.

--- R/datetime.R
+++ R/datetime.R
@@ -1,14 +1,16 @@
 # Copyright (c) Konrad Grzanek
 # Created 2017-06-02
 #
-
+#' @import stringr
 #' @import chR
 NULL
 
 #' Returns a timestamp string with the tzone appended.
 #' @export
-stampString <- function(tzone = "CEST") chString({
+stampString <- function(tzone = "CEST", hsep = "_") chString({
   chString(tzone)
-  paste0(format.POSIXct(Sys.time()), " ", tzone)
+  chString(hsep)
+  s <- paste0(format.POSIXct(Sys.time()), " ", tzone)
+  if (hsep == ":") s else str_replace_all(s, ":", hsep)
 })
 
