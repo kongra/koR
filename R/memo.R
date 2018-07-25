42eed618239178acf0f43ed364fff124ed6c8bb9
kongra <kongra@gmail.com>
2018-07-25 10:29:44 +0200

    Re-design.

--- R/memo.R
+++ R/memo.R
@@ -2,21 +2,9 @@
 # Created 2018-07-25
 #
 
-#' A fast memoizer for values of function call depending on some logical
-#' condition.
+#' A fast memoizer for values of depending on some logical condition.
 #' @export
-memoBool <- function(f, ...) {
-  chFun(f)
-  cache <- list(NULL, NULL)
-  function(b) {
-    idx <- if (b) 1L else 2L
-    v   <- cache[[idx]]
-    if (!is.null(v))
-      v
-    else {
-      v <- f(...)
-      cache[[idx]] <<- v
-      v
-    }
-  }
+memoBool <- function(forTrue, forFalse) {
+  cache <- list(forTrue, forFalse)
+  function(b) .subset2(cache, if (b) 1L else 2L)
 }
