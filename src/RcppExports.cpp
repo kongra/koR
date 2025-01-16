3d50357e313c9e442289ab17b9d1ce4976fb557c
Konrad Grzanek <kongra@gmail.com>
2025-01-16 16:24:38 +0100

    Updated RcppExport

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // boolsAndInterveawe
 LogicalVector boolsAndInterveawe(LogicalVector b, LogicalVector other);
 RcppExport SEXP _koR_boolsAndInterveawe(SEXP bSEXP, SEXP otherSEXP) {
